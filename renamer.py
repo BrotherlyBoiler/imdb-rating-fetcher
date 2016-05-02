322e893e98b7cb374a3c470676560841243c4414
Vishal Ponugoti <vponugot@purdue.edu>
2016-05-02 21:02:40 +0530

    Add ability to check if filename contains release year

--- renamer.py
+++ renamer.py
@@ -4,7 +4,7 @@
     to corresponding movie files/folders in the directory.
 
     File names are in one of the following formats:
-    1. P.S. I Love You.mkv
+    1. P.S. I Love You (7.1).mkv
     2. P.S. I Love You (2010).mp4
     3. P.S. I Love You (2010) [1080p].avi
 
@@ -14,9 +14,9 @@
 
     Ideally, the file in the directory would be renamed to one of
     the following:
-    1. P.S. I Love You (7.1).mkv
-    2. P.S. I Love You (2010) (7.1).mp4
-    3. P.S. I Love You (2010) [1080p] (7.1).avi
+    1. [IMDb 7.1] P.S. I Love You.mkv
+    2. [IMDb 7.1] P.S. I Love You (2010).mp4
+    3. [IMDb 7.1] P.S. I Love You (2010) [1080p].avi
 '''
 
 import os, json, urllib.request, re
@@ -26,33 +26,31 @@ base_uri = "http://www.omdbapi.com/?"
 query_title = "t="
 
 basepath = os.path.dirname(os.path.realpath(__file__))
-# basepath = "C:/Users/Vishal/Downloads/Movies/"
-# basepath = "E:/Movies"
 
 # Fetch movie rating from omdbapi.com. API return JSON format.
-def getRating(movie_title):
+def getMovieInfo(movie_title):
     movie_title = movie_title.strip().replace(" ", "+")
     # Request API for JSON response
     with urllib.request.urlopen(base_uri + query_title + movie_title) as url:
-        movie_data = json.loads(url.read().decode())    # decode to utf-8
-    if 'imdbRating' not in movie_data:
-        print ("Cannot find rating for " + movie_title)
+        data = json.loads(url.read().decode())    # decode to utf-8
+    if 'imdbRating' not in data or 'Year' not in data:
+        print ("Invalid response for " + movie_title)
         return None
-    return movie_data['imdbRating']
+    return [data['imdbRating'], data['Year']]
 
 # Checks if parameter file name already has a rating.
 # Movie ratings are in the format (\d.\d)
 def hasRating(filename):
     pattern = re.compile('\[IMDb [0-9].[0-9]\]')
-    if pattern.search(filename) is None:
-        return False
-    return True
+    return pattern.search(filename) is not None:
 
 def hasOldRating(filename):
     pattern = re.compile('\([0-9].[0-9]\)')
-    if pattern.search(filename) is None:
-        return False
-    return True
+    return pattern.search(filename) is not None:
+
+def hasYear(filename):
+    pattern = re.compile('\([0-9]{4}\)')
+    return pattern.search(filename) is not None:
 
 def removeOldRating(filename):
     index = filename.find('(')
@@ -71,6 +69,7 @@ def getMovieTitle(filename):
 
 def main():
     print ("Starting...")
+
     for filename in os.listdir(basepath):
         # Ignore hidden files and this script file
         if filename[0] is '.' or filename == __file__: continue
@@ -80,7 +79,7 @@ def main():
 
         movie_title = getMovieTitle(filename)
         file_ext = os.path.splitext(filename)[1]
-        movie_rating = getRating(movie_title)
+        movie_rating, movie_year = getMovieInfo(movie_title)
         # Continue if not rating is found
         if (movie_rating is None): continue
 
